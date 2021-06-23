5b74e1fb10aff4e2703fec695067380fbaeae174
This is a buffalo <52713465+tatazakup@users.noreply.github.com>
2021-06-23 22:50:31 +0700

    function view and receive information was completed

--- src/store/chat.module.js
+++ src/store/chat.module.js
@@ -4,29 +4,60 @@ export const chat = {
     namespaced: true,
     state: {
         loadingStatus: false,
-        socketID: '',
+        socket: null,
     },
     actions: {
-        async connectToIO ({ commit }, user) {
+        async connectToIO ({ commit }) {
+            await commit('chageLoadingStatus', true)
             await ChatService.initializer().then(
-                socket => {
-                    commit('InitialSocket', socket)
+                socketID => {
+                    commit('InitialSocket', socketID)
                     return Promise.resolve();
                 },
                 error => {
                     return Promise.reject(error);
                 }
             );
+            await commit('chageLoadingStatus', false)
+        },
+        async joinChatRoom ({ commit, state }) {
+            await commit('chageLoadingStatus', true)
+            await state.socket.emit('join', {
+                job_id: this.roomid,
+                user_id: this.getUser.id,
+            });
+            await commit('chageLoadingStatus', false)
+        },
+        async leaveChatRoom ({ commit, state }) {
+            await commit('chageLoadingStatus', true)
+            await state.socket.emit('leave', this.username)
+            await commit('chageLoadingStatus', false)
+        },
+        async receiveMessage ({ commit, state }) {
+            await commit('chageLoadingStatus', true)
+            await socket.on('recive-message', (data) => {
+                console.log(data);
+                  this.chat.push({
+                    self: false,
+                    message: data.message,
+                    avatar: data.avatar,
+                    createdAt: data.createdAt
+                  })
+              });
+            await commit('chageLoadingStatus', false)
         }
     },
     mutations: {
         InitialSocket({state}, input) {
-            state.socketID = input
-        }
+            state.socket = input
+        },
+        chageLoadingStatus({state}, input) {
+            state.loadingStatus = input
+        },
     },
     getters: {
         getSocketID(state) {
-            return state.socketID
+            return state.socket
         }
     }
 }
